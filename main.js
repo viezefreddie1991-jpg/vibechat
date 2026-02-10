3075ff9f0f2323f40e8f829947a935e446d00219
Franklin <franklinheetik@live.nl>
2026-02-10 21:08:20 +0100

    polish

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    // Load your LIVE hosted URL instead of localhost
+    win.loadURL('https://vibechat-mgc3.onrender.com');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
